c3771313200b1d6e34c1032976dfc2c712de4e2a
Jason Araos <jason.araos@preset.io>
2025-08-19 19:29:23 +0000

    deleting semantic yaml file

--- models/monthly_payment_summary.sql
+++ models/monthly_payment_summary.sql
@@ -1,4 +1,7 @@
-select * from {{ metrics.calculate(
-    [metric('total_revenue'), metric('payment_count')],
-    grain='month'
-) }}
+select
+    date_trunc('month', payment_date) as date_month,
+    sum(amount) as total_revenue,
+    count(payment_id) as payment_count
+from {{ ref('stg_pagila__payment') }}
+group by date_trunc('month', payment_date)
+order by date_month
