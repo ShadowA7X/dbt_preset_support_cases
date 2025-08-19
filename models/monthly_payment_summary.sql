6ee25993abfcf7a3e4f9cdb56303893c1e4bbc98
Jason Araos <jason.araos@preset.io>
2025-08-19 20:46:38 +0000

    Metric and semantic layer working (Finally!!!)

--- models/monthly_payment_summary.sql
+++
@@ -1,7 +0,0 @@
-select
-    date_trunc('month', payment_date) as date_month,
-    sum(amount) as total_revenue,
-    count(payment_id) as payment_count
-from {{ ref('stg_pagila__payment') }}
-group by date_trunc('month', payment_date)
-order by date_month
